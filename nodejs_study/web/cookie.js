bbd3462d7f433ba03c0167d8b499c0e1dd6056d8
otobashi <otobashi@gmail.com>
2015-02-23 20:14:48 +0900

    Add cookie

--- nodejs_study/web/cookie.js
+++ nodejs_study/web/cookie.js
@@ -3,18 +3,19 @@ var http = require('http');
 http.createServer(function(request,response){
 
 	var date = new Date();
-	date.setDate(date.getDate()+7);
 
-	response.writeHead(200,{
+	date.setDate(date.getDate() + 7);
+
+	response.writeHead(200, {
 		'Content-Type':'text/html',
 		'Set-cookie':[
-			'breakfast = toast;Expires = '+date.toUTCString(),
-			'dinner = chicken'
+			'breakfast = milk;Expired = ' + date.toUTCString(),
+			'lunch = banana',
+			'dinner = orange'
 		]
 	});
 
 	response.end('<h1>' + request.headers.cookie + '</h1>');
-
-}).listen(52273,function(){
+}).listen(52273, function(){
 	console.log('Server Running at http://127.0.0.1:52273');
-});
+})
