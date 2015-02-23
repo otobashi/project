b772d778fe53b073eb94065922327d54bb95222b
otobashi <otobashi@gmail.com>
2015-02-23 23:17:27 +0900

    Study IF

--- nodejs_study/web/app.js
+++ nodejs_study/web/app.js
@@ -7,16 +7,20 @@ http.createServer(function(request,response){
 	var pathname = url.parse(request.url).pathname;
 
 	if (pathname == '/'){
-		fs.readFile('index.html', function(error,data){
-			response.writeHead(200, {'Content-Type':'text/html'});
+
+		fs.readFile('index.html',function(error,data){
+			response.writeHead(200,{'Content-Type':'text/html'});
 			response.end(data);
 		});
+
 	}
 	else if (pathname == '/OtherPage'){
+
 		fs.readFile('OtherPage.html',function(error,data){
-			response.writeHead(200, {'Content-Type':'text/html'});
+			response.writeHead(200,{'Content-Type':'text/html'});
 			response.end(data);
 		});
+
 	}
 }).listen(52273, function(){
 	console.log('Server Running at http://127.0.0.1:52273');
