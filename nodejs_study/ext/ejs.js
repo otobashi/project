9dff71374348c0159946bd8299e99c9bb1affd2e
otobashi <otobashi@gmail.com>
2015-02-24 21:21:56 +0900

    Study els

--- nodejs_study/ext/ejs.js
+++ nodejs_study/ext/ejs.js
@@ -1,2 +1,12 @@
-var ejs = require('ejs');
-var jade = require('jade');
+var http = require('http');
+var fs   = require('fs');
+var ejs  = require('ejs');
+
+http.createServer(function(request,response){
+	fs.readFile('ejsPage.ejs','utf8',function(error,data){
+		response.writeHead(200, {'Content-Type':'text/html'});
+		response.end(ejs.render(data));
+	});
+}).listen(52273,function(){
+	console.log('Server Running at http://127.0.0.1:52273');
+});
